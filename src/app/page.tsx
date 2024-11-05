d17d42ceaea1464b18c232af510b206d88f29a61
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-11-05 22:20:06 +0530

    Clerk auth and protecting routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import Link from "next/link";
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -7,6 +14,12 @@ export default function Home() {
     <main className="flex flex-col justify-center h-screen text-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
+        <SignedOut>
+          <SignInButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign In</Link>
         </Button>
