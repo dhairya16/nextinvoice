438c8c7c5756fe80ad80e693b11feae6f196a805
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-11-07 20:13:35 +0530

    add userId in schema

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,17 +1,21 @@
 "use server";
-import { redirect } from "next/navigation";
+import {redirect} from "next/navigation";
+import {auth} from "@clerk/nextjs/server";
 
-import { Invoices } from "@/db/schema";
-import { db } from "@/db";
+import {Invoices} from "@/db/schema";
+import {db} from "@/db";
 
 export async function createAction(formData: FormData) {
+  const {userId} = await auth();
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
 
+  if (!userId) return;
+
   const results = await db
     .insert(Invoices)
-    .values({ value, description, status: "open" })
-    .returning({ id: Invoices.id });
+    .values({value, description, userId, status: "open"})
+    .returning({id: Invoices.id});
 
-  redirect(`invoices/${results[0].id}`);
+  redirect(`/invoices/${results[0].id}`);
 }
